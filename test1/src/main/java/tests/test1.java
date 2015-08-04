7e01cfbcebd51fdca7e9b77863ed714b89c69cfc
root <root@debian>
2015-08-04 12:19:07 +0200

    test 2

---
+++ test1/src/main/java/tests/test1.java
@@ -0,0 +1,23 @@
+package tests;
+import com.mycompany.test1.*;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class test1 {
+    
+    @Test
+    public void testAdd(){
+      Bank banca = new Bank(10);
+      double amount = banca.add(5);
+      Assert.assertEquals(15,amount);
+    }
+      @Test
+      public void testSub(){
+        Bank banca = new Bank(10);
+        double amount = banca.sub(5);
+        Assert.assertEquals(5, amount);
+    }
+      
+}
+    
+
